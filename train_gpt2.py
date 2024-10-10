ca298b44d3e2b474ae8d07d1cfb023f528e03641
Sagar <jpsagarm95@gmail.com>
2024-10-09 22:23:26 -0700

    Add learning rate scheduler

--- train_gpt2.py
+++ train_gpt2.py
@@ -214,9 +214,23 @@ model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 model = torch.compile(model)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+def get_lr(it):
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+    if it > max_steps:
+        return min_lr
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
 
-for i in range(50):
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -227,12 +241,16 @@ for i in range(50):
         with torch.autocast(device_type=device, dtype=torch.bfloat16):
             logits, loss = model(x, y)
     loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     # torch.cuda.synchronize()
     t1 = time.time()
-    dt = (t1 - t0)*1000
+    dt = (t1 - t0)
     tokens_per_sec = (train_loader.B * train_loader.T / (t1 - t0))
-    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, token_per_sec: {tokens_per_sec:.2f}")
+    print(f"step {step:4d} | loss: {loss.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | token_per_sec: {tokens_per_sec:.2f}")
 
 
 # num_return_sequences = 5
