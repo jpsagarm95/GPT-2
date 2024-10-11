babb6fc2e84c293b1d590b50340f4766736b3274
Sagar <jpsagarm95@gmail.com>
2024-10-10 23:41:34 -0700

    Fix loss printing bug

--- train_gpt2.py
+++ train_gpt2.py
@@ -286,7 +286,7 @@ for step in range(max_steps):
     dt = (t1 - t0)
     tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
     tokens_per_sec = tokens_processed / dt
-    print(f"step {step:4d} | loss: {loss.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | token_per_sec: {tokens_per_sec:.2f}")
+    print(f"step {step:4d} | loss: {loss_accum.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | token_per_sec: {tokens_per_sec:.2f}")
 
 
 # num_return_sequences = 5
